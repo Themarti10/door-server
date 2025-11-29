b0bea55c14ed94aea799727282e0be565f8cec45
Amirlan Mukhanbetkaliyev <amirlanm251000@gmail.com>
2025-11-29 22:49:05 +0600

    Russian version with ISO dates

--- server.js
+++ server.js
@@ -13,14 +13,8 @@ let eventLog = [];
 // POST от ESP32
 app.post("/api", (req, res) => {
   const { door } = req.body;
-  const timestamp = new Date().toLocaleString('ru-RU', {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit'
-  });
+  // Отправляем ISO-формат, чтобы браузер корректно парсил
+  const timestamp = new Date().toISOString();
   const event = { door, timestamp };
   eventLog.push(event);
   console.log("Получено событие:", event);
